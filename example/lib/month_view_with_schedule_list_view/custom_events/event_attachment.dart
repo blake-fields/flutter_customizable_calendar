d0919b618ce7d0414b6e9ba6c1f3f5c613245f52
shawnfields <shawn.b.fields4@gmail.com>
2025-02-20 15:38:32 -0600

    Intl version upgrade

--- example/lib/month_view_with_schedule_list_view/custom_events/event_attachment.dart
+++
@@ -1,9 +0,0 @@
-class EventAttachment {
-  const EventAttachment({
-    required this.title,
-    required this.iconAsset,
-  });
-
-  final String title;
-  final String iconAsset;
-}
--- example/lib/utils.dart
+++
@@ -1,5 +0,0 @@
-extension DoubleExtension on double {
-  bool comparePrecision(double value, {double precision = 0.01}) {
-    return (this - value) < precision;
-  }
-}
